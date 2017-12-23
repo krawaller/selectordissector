e43aa950d2f15ca70e90e78756bc403ae0bf809e
David Waller <david@krawaller.se>
2017-12-23 06:22:08 +0100

    test nth-child validation

--- test/validator/pseudo.spec.ts
+++ test/validator/pseudo.spec.ts
@@ -1,7 +1,7 @@
 import * as test from 'tape';
 
 import {QueryError} from '../../types';
-import validator, {isPseudoSelector, hasPseudoSelector, unknownPseudoSelector} from '../../validator';
+import validator, {isPseudoSelector, hasPseudoSelector, unknownPseudoSelector, nthOfTypeDataError} from '../../validator';
 
 test(`Experimental pseudo selectors aren't allowed`, t => {
   type TestCase = [string, QueryError, number[]];
@@ -13,6 +13,21 @@ test(`Experimental pseudo selectors aren't allowed`, t => {
     validator(query),
     [error, position],
     `Query ${query} gives experimental selector error for position ${position}`
+  ));
+  t.end();
+});
+
+test(`nth-of-type data must be correct`, t => {
+  type TestCase = [string, number[], string];
+  const nthCases: TestCase[] = [
+    ['div p span:nth-of-type', [5], 'we get error when we have no data parens'],
+    ['div :nth-of-type(foo)', [2], 'we get error when we have nonsense data'],
+    [':nth-of-type(0)', [0], 'we get error when we have 0 as data']
+  ];
+  nthCases.forEach(([query, position, description]) => t.deepEqual(
+    validator(query),
+    [nthOfTypeDataError, position],
+    description
   ));
   t.end();
 });
