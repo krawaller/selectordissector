1c70d6b3b4e83f5bea36a18c1912588521539873
David Waller <david@krawaller.se>
2018-02-07 14:47:57 +0100

    allow builder as children arg to builder

--- src/builder/index.ts
+++ src/builder/index.ts
@@ -3,29 +3,52 @@ import {VirtualElement} from '../types';
 type Attrs = {[key:string]: string};
 
 const elemProto = {};
+const builderWatermark = Symbol('builder');
 
-const builderFactory = type => (attrParam: Attrs | VirtualElement[] | string = {}, childrenParam: VirtualElement[] | string = []) => {
-  let children: VirtualElement[];
-  let content: string;
-  if (Array.isArray(attrParam) || typeof attrParam === 'string'){
-    childrenParam = attrParam;
-    attrParam = {};
+type ChildrenType = VirtualElement[] | string | VirtualElement | Builder;
+
+type Builder = {
+  (a?: Attrs | ChildrenType, c?: ChildrenType): VirtualElement
+  watermark?: Symbol
+};
+
+type Factory = (type: string) => Builder;
+
+const builderFactory: Factory = type => {
+  const builder: Builder = (attrParam = {}, childrenParam = []) => {
+    let children: VirtualElement[];
+    let content: string;
+    if (Array.isArray(attrParam) || typeof attrParam === 'string' || isElem(attrParam) || isBuilder(attrParam)){
+      childrenParam = attrParam;
+      attrParam = {};
+    }
+    if (typeof childrenParam === 'string'){
+      content = childrenParam;
+      children = [];
+    } else if(isElem(childrenParam)) {
+      children = [childrenParam];
+    } else if (isBuilder(childrenParam)) {
+      children = [childrenParam()];
+    } else {
+      children = childrenParam;
+    }
+    content = content || '';
+    // add elemProto so that we can identify objects as elements in the isElem function
+    return Object.assign(Object.create(elemProto),{ type, attrs: attrParam, children, content });
   }
-  if (typeof childrenParam === 'string'){
-    content = childrenParam;
-    children = [];
-  } else {
-    children = childrenParam;
-    content = '';
-  }
-  // add elemProto so that we can identify objects as elements in the isElem function
-  return Object.assign(Object.create(elemProto),{ type, attrs: attrParam, children, content });
+  // add watermark so we can identify functions as builders in the isBuilder func
+  builder.watermark = builderWatermark;
+  return builder;
 }
 
 export default builderFactory;
 
-export function isElem(elem){
+export function isElem(elem): elem is VirtualElement {
   return elemProto.isPrototypeOf(elem);
+}
+
+export function isBuilder(func): func is Builder {
+  return func.watermark === builderWatermark;
 }
 
 export const div = builderFactory('div');
--- test/builder/builder.spec.ts
+++ test/builder/builder.spec.ts
@@ -1,9 +1,15 @@
 import * as test from 'tape';
 
 import {VirtualElement} from '../../src/types';
-import builderFactory, {isElem, div} from '../../src/builder';
+import builderFactory, {isElem, isBuilder, div} from '../../src/builder';
 
-test('The isElem factory', t => {
+test('The isBuilder tester', t => {
+  t.equal(isBuilder(()=>{}), false, 'returns false for regular func');
+  t.equal(isBuilder(div), true, 'returns true for builder func');
+  t.end();
+});
+
+test('The isElem tester', t => {
   t.equal(isElem({foo:'bar'}), false, 'returns false for regular object');
   t.equal(isElem(div()), true, 'returns true for elem object');
   t.end();
@@ -23,13 +29,24 @@ test('The builder factory', t => {
       {type: 'p', attrs: {}, content: '', children: [
         {type: 'strong', attrs: {}, content: 'moo', children: []}
       ]},
-      'b([child]) gives correct wrapping'
+      'b([child()]) gives correct wrapping'
     ], [
       p({lang:'sv'}),
       {type: 'p', attrs: {lang: 'sv'}, content: '', children: []},
       'b(attrs) gives the attrs and no content or children'
+    ], [
+      p(strong('wee')),
+      {type: 'p', attrs: {}, content: '', children: [
+        {type: 'strong', attrs: {}, content: 'wee', children: []}
+      ]},
+      'b(child()) gives the child in children array correctly'
+    ],[
+      p(strong),
+      {type: 'p', attrs: {}, content: '', children: [
+        {type: 'strong', attrs: {}, content: '', children: []}
+      ]},
+      'b(child) gives the child in children array correctly'
     ]
-
   ];
   builds.forEach(([input,output,desc]) => t.deepEqual(input,output,desc));
   t.end();
