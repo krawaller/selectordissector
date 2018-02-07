1b4d918d17a2a20c6217a0f556f28544e89d4291
David Waller <david@krawaller.se>
2018-02-07 12:21:37 +0100

    use enum in combinefrmopath switch

--- src/matcher/combineFromPath.ts
+++ src/matcher/combineFromPath.ts
@@ -1,18 +1,18 @@
-import {VirtualElement, CombinatorToken} from '../types';
+import {VirtualElement, CombinatorToken, TokenType} from '../types';
 import {getDescendantPaths, travelTree} from '../helpers';
 
 export default function combineFromPath(tree: VirtualElement, path: number[], token: CombinatorToken){
   switch(token.type){
-    case 'descendant': {
+    case TokenType.descendant: {
       return getDescendantPaths(tree, path, true);
     }
-    case 'child': {
+    case TokenType.child: {
       return (travelTree(tree, path).children || []).map((c,i) => path.concat(i))
     }
-    case 'adjacent': {
+    case TokenType.adjacent: {
       return path[path.length-1] ? [path.slice(0,path.length-1).concat( path[path.length-1] - 1)] : [];
     }
-    case 'sibling': {
+    case TokenType.sibling: {
       return Array.from(Array(path[path.length-1]||0).keys()).map(n => path.slice(0,path.length-1).concat(n));
     }
   }
