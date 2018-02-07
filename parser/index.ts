9fc3af016d431d920d8f0be1eac0dc7885700f46
David Waller <david@krawaller.se>
2018-02-07 11:13:49 +0100

    type for selector

--- parser/index.ts
+++ parser/index.ts
@@ -1,7 +1,7 @@
 import * as cssWhat from 'css-what';
 
-import {QueryToken} from '../types';
+import {Selector} from '../types';
 
-export default function (query: string): QueryToken[][] {
+export default function (query: string): Selector[] {
   return cssWhat(query);
 }
--- types/index.ts
+++ types/index.ts
@@ -3,3 +3,7 @@ export * from './velement';
 
 export type Path = number[];
 export type Collection = Path[];
+
+import {QueryToken} from './querytoken';
+
+export type Selector = QueryToken[];
