350e46d716d4a762da0e956bcb74a742c83a3ee8
David Waller <david@krawaller.se>
2018-02-07 07:59:02 +0100

    test finding of unknown pseudo names

--- test/validator/pseudo.spec.ts
+++ test/validator/pseudo.spec.ts
@@ -30,3 +30,19 @@ test(`nth-of-type data must be correct`, t => {
   ));
   t.end();
 });
+
+
+test(`unrecognized pseudo selectors are called out`, t => {
+  type TestCase = [string, number[], string];
+  const nthCases: TestCase[] = [
+    ['div p span:foo', [5], 'unknown pseudo foo is correctly called out'],
+    ['div :bar', [2], 'unknown pseudo bar is correctly called out'],
+    [':baz', [0], 'unknown pseudo baz is correctly called out']
+  ];
+  nthCases.forEach(([query, position, description]) => t.deepEqual(
+    validator(query),
+    [QueryError.unknownPseudoSelector, position],
+    description
+  ));
+  t.end();
+});
