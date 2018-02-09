9743b580b2643a2e4c1c2fedfb5fc2bc1affceea
David Waller <david@krawaller.se>
2018-02-09 12:07:56 +0100

    describeError func

---
+++ src/helpers/describeError.ts
@@ -0,0 +1,18 @@
+import {QueryError} from '../types';
+
+export function describeError(error: QueryError): string {
+  switch(error){
+    case QueryError.adjacentCombinators: return `A combinator cannot follow on another combinator`;
+    case QueryError.endingCombinator: return `Your selector cannot end with a combinator`;
+    case QueryError.extraneousParens: return `This pseudo selector should not have parenthesis`;
+    case QueryError.faultyFormula: return `The pseudo selector argument is invalid`;
+    case QueryError.hasPseudoSelector: return `We haven't implemented the :has selector`;
+    case QueryError.isPseudoSelector: return `We haven't implemented the :is selector`;
+    case QueryError.leadingCombinator: return `Your selector cannot begin with a combinator`;
+    case QueryError.missingParens: return `This pseudo selector should have parenthesis`;
+    case QueryError.parentCombinator: return `We have chosen not to implement the parent combinator`;
+    case QueryError.parseError: return `Couldn't parse the selector`;
+    case QueryError.unImplemented: return `We haven't implemented this pseudo selector`;
+    case QueryError.unknownPseudoSelector: return `We haven't implemented this pseudo selector`;
+  }
+}
