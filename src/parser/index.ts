20b1d4809dd388a4157521450af21b3c907a5538
David Waller <david@krawaller.se>
2018-03-08 08:12:32 +0100

    move validation to parser

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,25 +1,41 @@
 import * as cssWhat from "css-what";
 
-import {QueryError, Selector, TokenType} from "../types";
+import {ErrorToken, QueryError, Selector, TokenType} from "../types";
+
+import {travelArray} from "../helpers";
+
+import {validateSelector} from "../validator";
 
 const residue = /\.$|\:$|\[[^\]]*$|#$/g;
 
 export default function(query: string): Selector[] {
   const wip = query.match(residue);
+  let result: Selector[];
   try {
-    const result = cssWhat(query.replace(residue, ""));
-    if (wip) {
-      result[0].push({
-        type: TokenType.wip,
-        value: wip[0],
-      });
-    }
-    return result;
+    result = cssWhat(query.replace(residue, ""));
   } catch (e) {
     return [[{
       name: QueryError.parseError,
       type: TokenType.error,
-      value: query,
+      value: {
+        type: TokenType.unparsed,
+        value: query,
+      },
     }]];
   }
+  const validation = validateSelector(result[0]);
+  if (validation) {
+    const [error, selector, path] = validation;
+    result[0] = result[0].slice(0, path[0]).concat({
+      name: error,
+      type: TokenType.error,
+      value: travelArray(selector, path),
+    } as ErrorToken);
+  } else if (wip) {
+    result[0].push({
+      type: TokenType.wip,
+      value: wip[0],
+    });
+  }
+  return result;
 }
--- test/parser/parsing.spec.ts
+++ test/parser/parsing.spec.ts
@@ -53,13 +53,38 @@ test("Parsing nonsense yields an error token", (t) => {
   const nonsenses: TestCase[] = [
     [
       ":#",
-      {type: TokenType.error, name: QueryError.parseError, value: ":#"},
+      {name: QueryError.parseError, type: TokenType.error, value: {
+        type: TokenType.unparsed,
+        value: ":#",
+      }},
       "we correctly cast nonsense to parserror token",
     ],
   ];
   nonsenses.forEach(([input, error, desc]) => t.deepEqual(
     parser(input),
     [[error]],
+    desc,
+  ));
+  t.end();
+});
+
+test("Parsing errors are added to selector", (t) => {
+  type TestCase = [string, Selector, string];
+  const errors: TestCase[] = [
+    [
+      "div < span",
+      [
+        {type: TokenType.tag, name: "div"},
+        {name: QueryError.parentCombinator, type: TokenType.error, value: {
+          type: TokenType.parent,
+        }},
+      ],
+      "we correctly get error replacement",
+    ],
+  ];
+  errors.forEach(([input, selector, desc]) => t.deepEqual(
+    parser(input),
+    [selector],
     desc,
   ));
   t.end();
