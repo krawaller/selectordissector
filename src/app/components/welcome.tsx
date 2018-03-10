a20def4802a3564741ee821210adfa2e1dec03d7
David Waller <david@krawaller.se>
2018-03-10 11:21:42 +0100

    fix typo in welcome message

--- src/app/components/welcome.tsx
+++ src/app/components/welcome.tsx
@@ -7,7 +7,7 @@ const Welcome: React.StatelessComponent<null> = () => (
     <Typography use="title" tag="h2">Welcome!</Typography>
     <Typography use="body1">
       Enter a selector to get a step-by-step explanation of how it works,
-      and which element are caught each step!
+      and which elements are caught for each step!
     </Typography>
   </div>
 );
