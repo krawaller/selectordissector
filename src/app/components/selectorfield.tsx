658af9d2f4496cb1ed68ff7cf8ecba66de1febd2
David Waller <david@krawaller.se>
2018-03-10 23:08:40 +0100

    solidify rmwc bug fix

--- src/app/components/selectorfield.tsx
+++ src/app/components/selectorfield.tsx
@@ -14,6 +14,17 @@ export default class SelectorField extends React.Component<SelectorFieldProps, n
     const input: HTMLInputElement = ReactDOM.findDOMNode(this.field).querySelector("input[type=text]");
     input.focus();
   }
+  // Hack to compensate for RMWC bug where label will erroneously lose float-above class
+  public componentDidUpdate() {
+    const label: HTMLLabelElement = ReactDOM.findDOMNode(this.field).querySelector("label");
+    setTimeout(() => {
+      if (this.props.query) {
+        label.classList.add("mdc-text-field__label--float-above");
+      } else {
+        label.classList.remove("mdc-text-field__label--float-above");
+      }
+    }, 50);
+  }
   public render() {
     return (
       <TextField
