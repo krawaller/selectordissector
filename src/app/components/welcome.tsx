cf810885cf1176a0e95fa55fd8b38f38a5bd0066
David Waller <david@krawaller.se>
2018-03-10 08:08:39 +0100

    welcome text

--- src/app/components/welcome.tsx
+++ src/app/components/welcome.tsx
@@ -3,7 +3,13 @@ import * as React from "react";
 import { Typography } from "rmwc/Typography";
 
 const Welcome: React.StatelessComponent<null> = () => (
-  <Typography use="body1">Welcome! Enter a selector above to get started.</Typography>
+  <div>
+    <Typography use="title" tag="h2">Welcome!</Typography>
+    <Typography use="body1">
+      Enter a selector to get a step-by-step explanation of how it works,
+      and which element are caught each step!
+    </Typography>
+  </div>
 );
 
 export default Welcome;
