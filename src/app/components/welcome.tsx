646f58060b9a449c1de8c695b8ddadfe3345a838
David Waller <david@krawaller.se>
2018-03-11 15:34:13 +0100

    debusify welcome

--- src/app/components/welcome.tsx
+++ src/app/components/welcome.tsx
@@ -4,12 +4,11 @@ import { Typography } from "rmwc/Typography";
 import {accentTextStyles} from "../styles";
 
 const Em: React.StatelessComponent<null> = ({children}) => (
-  <span style={accentTextStyles}>{children}</span>
+  <React.Fragment>{children}</React.Fragment> // <span style={accentTextStyles}>{children}</span>
 );
 
 const Welcome: React.StatelessComponent<null> = () => (
   <div>
-    <Typography use="title" tag="h3">Hello! :)</Typography>
     <Typography use="body1" tag="p">
       <Em>Enter a selector</Em> to get a <Em>step-by-step explanation</Em> of how it works,
       and see <Em>which elements are caught</Em> for each step!
