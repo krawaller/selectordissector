a7bc55cc4384fbf3d42ce5649f9cb7b713e66a81
David Waller <david@krawaller.se>
2018-03-11 11:06:25 +0100

    update rmwc, use local mc sheet

--- setupdist.js
+++ setupdist.js
@@ -4,3 +4,8 @@ var fs = require("fs-extra");
 
 fs.emptyDirSync(__dirname + "/dist");
 fs.copySync(__dirname + "/src/app/index.html", __dirname + "/dist/index.html");
+
+fs.copySync(
+  __dirname + "/node_modules/material-components-web/dist/material-components-web.min.css",
+  __dirname + "/dist/material-components-web.min.css",
+);
